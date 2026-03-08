6c88a2012860da1f70c73b15ef6e74f3146f4123
truongbt <bui.tuan.truong@sun-asterisk.com>
2026-03-08 19:39:30 +0700

    use env firebase

--- backend/src/config/firebase.js
+++ backend/src/config/firebase.js
@@ -1,12 +1,12 @@
 const admin = require('firebase-admin');
-const path = require('path');
 
 if (!admin.apps.length) {
-  const serviceAccountPath = process.env.FIREBASE_SERVICE_ACCOUNT_PATH;
   admin.initializeApp({
-    credential: admin.credential.cert(
-      path.resolve(serviceAccountPath)
-    ),
+    credential: admin.credential.cert({
+      projectId: process.env.FIREBASE_PROJECT_ID,
+      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    }),
   });
 }
 
